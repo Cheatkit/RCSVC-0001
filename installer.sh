46f34f823a2e0e2216a1f66e42335759699a7a30
CastouloLee <125958687+CastouloLee@users.noreply.github.com>
2023-05-12 17:33:17 +0200

    installer.sh finished

--- installer.sh
+++ installer.sh
@@ -5,20 +5,52 @@
 git clone --depth 1 https://github.com/rcprc/rcsvc-0001.git /tmp/repo
 
 # Make directory in /usr/local/bin (silent)
+echo -en "Creating directory in /usr/local/bin/."
+sleep 0.5
+echo -en "Creating directory in /usr/local/bin/.."
+sleep 0.5
+echo -en "Creating directory in /usr/local/bin/..."
+sleep 0.5
+echo -en "Creating directory in /usr/local/bin/."
+sleep 0.5
+echo -en "Creating directory in /usr/local/bin/.."
+sleep 0.5
+echo -en "Created directory in /usr/local/bin named logkeeper"
 sudo mkdir /usr/local/bin/logkeeper /dev/null 2>&1
 
 
+
 # Move necessary files to /usr/local/bin (silent)
+echo -en "Moving files."
+sleep 0.5
+echo -en "Moving files.."
+sleep 0.5
+echo -en "Moving files..."
 sudo mv /tmp/tepo/file /usr/local/bin/logkeeper /dev/null 2>&1
+
 
 # Give all .sh files executable permissions
 sudo chmod +x /usr/local/bin/logkeeper/*.sh
 
 # create countfile
+echo -en "Creating Countfile."
+sleep 0.5
+echo -en "Creating Countfile.."
+sleep 0.5
+echo -en "Creating Countfile..."
+sleep 0.5
+echo -en "Creating Countfile."
 sudo touch countfile /usr/local/bin/logkeeper/countfile /dev/null 2>&1
 
 # Copy the service file to destination
-cp logkeeper.service /etc/systemd/system /dev/null 2>&1
+echo "Moving .service file."
+sleep 0.5
+echo "Moving .service file.."
+sleep 0.5
+echo "Moving .service file..."
+sleep 0.5
+echo "Moving .service file."
+sudo mv /tmp/repo/logkeeper.service /etc/systemd/system /dev/null 2>&1
 
 # Reload systemd configuration
 systemctl daemon-reload
